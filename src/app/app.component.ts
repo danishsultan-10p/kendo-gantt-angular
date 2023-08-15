3082d928646b829568a7001be67efaea9eefd06d
danishsultan-10p <danish.sultan@10pearls.com>
2023-08-15 18:13:12 +0500

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {
   GanttDependency,
   TimelineViewType,
 } from '@progress/kendo-angular-gantt';
+import { DateFormatOptions } from '@progress/kendo-angular-intl';
 import { Task, tasks, dependencies } from './hierarchical-data';
 
 @Component({
@@ -13,4 +14,9 @@ export class AppComponent {
   public data: Task[] = tasks;
   public dependencies: GanttDependency[] = dependencies;
   public activeView: TimelineViewType = 'week';
+
+  timelineHeadersDateFormat: DateFormatOptions = {
+    groupHeaderDateFormat: 'wdc k  E hh:mm a',
+    columnHeaderDateFormat: 'E',
+  };
 }
